a615c15429164845eb0d1fb5d7fcc90efa100f0c
daeh <daeh@tuta.io>
2018-12-26 13:31:43 +0100

    improved install script

--- install.sh
+++ install.sh
@@ -1,94 +1,73 @@
 #!/bin/sh
-
-echo "Checking for QT dependencies"
-echo ""
-
+if [ "$EUID" -ne 0 ]; then
+    echo enter password in order to install Yin-Yang correctly
+    sudo sh ./$0
+    exit 1
+fi
 echo "Installing Yin-Yang ..."
+echo ""
+echo "Checking for QT dependencies"
 echo ""
 #checking python dependencies
 pip3 install qtpy
 pip3 install pyqt5
 echo ""
-
-if [ "$EUID" -ne 0 ]; then
-    echo run this script as root
-    exit 1
-fi
 echo "Checking and creating correct folders ..."
-
 #check if /opt/ directory exists else create
 if [ ! -d /opt/ ]; then
     mkdir /opt/
 fi
-
 #check if /opt/ directory exists else create
 if [ ! -d /opt/yin-yang/ ]; then
     mkdir /opt/yin-yang/
 fi
-
 echo ""
 echo "done"
 echo ""
-
 echo "Installin yin-yang for Commandline usage"
 # copy files
 cp -r ./* /opt/yin-yang/
-
-
 #copy terminal executive
 cp ./bin/yin-yang /usr/bin/
 sudo chmod +x /usr/bin/yin-yang
-
-
 echo "Creating .desktop file for native enviroment execution"
-
 #create .desktop file
 cat <<EOF >/home/$SUDO_USER/.local/share/applications/Yin-Yang.desktop
 [Desktop Entry]
-
 # The type as listed above
 Type=Application
-
 # The version of the desktop entry specification to which this file complies
 Version=0.0.1
-
 # The name of the application
 Name=Yin & Yang
-
 # A comment which can/will be used as a tooltip
-Comment=Auto Nightmode for KDE and VSCode 
-
+Comment=Auto Nightmode for KDE and VSCode
 # The path to the folder in which the executable is run
 Path=/opt/yin-yang
-
 # The executable of the application, possibly with arguments.
 Exec=sh yin-yang
-
 # The name of the icon that will be used to display this entry
 Icon=/opt/yin-yang/bin/ui/assets/icon.png
-
 # Describes whether this application needs to be run in a terminal or not
 Terminal=false
-
 # Describes the categories in which this entry should be shown
 Categories=Education;Languages;Python; Cool;
 EOF
 
 cat << "EOF"
-
- __     ___          __     __               
- \ \   / (_)         \ \   / /               
-  \ \_/ / _ _ __ _____\ \_/ /_ _ _ __   __ _ 
+ __     ___          __     __
+ \ \   / (_)         \ \   / /
+  \ \_/ / _ _ __ _____\ \_/ /_ _ _ __   __ _
    \   / | | '_ \______\   / _` | '_ \ / _` |
     | |  | | | | |      | | (_| | | | | (_| |
     |_|  |_|_| |_|      |_|\__,_|_| |_|\__, |
                                         __/ |
-                                       |___/ 
+                                       |___/
 EOF
-
-
+echo ""
+echo "Yin-Yang brings Auto Nightmode for KDE and VSCode"
+echo ""
 cat << "EOF"
-
        _..oo8"""Y8b.._
      .88888888o.    "Yb.
    .d888P""Y8888b      "b.
@@ -102,12 +81,9 @@ cat << "EOF"
    "Y88888b     ""     oP"
      "Y8888o._     _.oP"
        `""Y888boodP""'
-       
-EOF
 
+EOF
 echo ""
 echo ""
 echo "checkout https://github.com/daehruoydeef/Yin-Yang for help"
 echo "Yin-Yang is now installed"
-
-echo "Yin-Yang brings Auto Nightmode for KDE and VSCode"
