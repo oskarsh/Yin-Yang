c8e82235d7694d765aa7a040f1083e4c8b3c7dc7
Oskar <daeh@daeh-fedora.fritz.box>
2019-03-09 22:22:15 +0100

    added stable wallpaper support

--- bin/change_wallpaper.sh
+++ bin/change_wallpaper.sh
@@ -1,10 +1,10 @@
 dbus-send --session --dest=org.kde.plasmashell --type=method_call /PlasmaShell org.kde.PlasmaShell.evaluateScript 'string:
-var Desktops = desktops();
+var Desktops = desktops();                                                                                                                       
 for (i=0;i<Desktops.length;i++) {
         d = Desktops[i];
         d.wallpaperPlugin = "org.kde.image";
         d.currentConfigGroup = Array("Wallpaper",
                                     "org.kde.image",
                                     "General");
-        d.writeConfig("Image", "file:$wallpaper");
+        d.writeConfig("Image", "file:'$1'");
 }'
--- bin/plugins/wallpaper.py
+++ bin/plugins/wallpaper.py
@@ -1,44 +1,36 @@
 import subprocess
 from bin import config
+import os
+import pwd
 
 
 def switchToDark():
+
     wallpaper_dark = config.get("wallpaperDarkTheme")
+
+    string = """'string:
+    var Desktops = desktops();                                                                                                                       
+    for (i=0;i<Desktops.length;i++) {
+            d = Desktops[i];
+            d.wallpaperPlugin = "org.kde.image";
+            d.currentConfigGroup = Array("Wallpaper",
+                                        "org.kde.image",
+                                        "General");
+            d.writeConfig("Image", "file:"""+wallpaper_dark+"""");
+    }'"""
+
+    print(string)
+
     if wallpaper_dark == "":
         subprocess.run(["notify-send", "looks like no dark wallpaper is set"])
     else:
-        inplace_change("./bin/change_wallpaper.sh", "$wallpaper", wallpaper_dark)
-        subprocess.run(["sh", "./bin/change_wallpaper.sh"])
-        inplace_change("./bin/change_wallpaper.sh", wallpaper_dark, "$wallpaper")
+        subprocess.run(["sh", "/opt/yin-yang/bin/change_wallpaper.sh", wallpaper_dark])
 
 
 def switchToLight():
     wallpaper_light = config.get("wallpaperLightTheme")
+
     if wallpaper_light == "":
         subprocess.run(["notify-send", "looks like no light wallpaper is set"])
     else:
-        inplace_change("./bin/change_wallpaper.sh", "$wallpaper", wallpaper_light)
-        subprocess.run(["sh", "./bin/change_wallpaper.sh"])
-        inplace_change("./bin/change_wallpaper.sh", wallpaper_light, "$wallpaper")
-
-
-def inplace_change(filename, old_string, new_string):
-    #
-    # @params: config - config to be written into file
-    #          path - the path where the config is will be written into
-    #           defaults to the default path
-
-    # Safely read the input filename using 'with'
-    with open(filename) as f:
-        s = f.read()
-        if old_string not in s:
-            print('"{old_string}" not found in {filename}.'.format(**locals()))
-            return
-
-    # Safely write the changed content, if found in the file
-    with open(filename, 'w') as f:
-        print(
-            'Changing "{old_string}" to "{new_string}" in {filename}'
-            .format(**locals()))
-        s = s.replace(old_string, new_string)
-        f.write(s)
+        subprocess.run(["sh", "/opt/yin-yang/bin/change_wallpaper.sh", wallpaper_light])
