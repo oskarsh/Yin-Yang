34a4c9fe8eaccd646f01741fef31da1f598f3ac6
l0drex <sunshine.lorenz@web.de>
2020-04-19 14:07:44 +0200

    Some fixes for installation scripts

--- uninstall.sh
+++ uninstall.sh
@@ -1,23 +1,25 @@
-#!/bin/#!/src/sh
+#!/bin/sh
 
 # this script will uninstall Yin-Yang and will also delete its config files
 
+# check, if sudo
+if [ "$EUID" -ne 0 ]; then
+    echo enter password in order to install Yin-Yang correctly
+    sudo sh $0
+    exit 1
+fi
+
 echo "your current user is in uninstall" $SUDO_USER
 
-if [ -z "$1" ]
-  then
-    rm -rf /home/$SUDO_USER/.local/share/applications/Yin-Yang.desktop
-    rm -rf /home/$SUDO_USER/.config/yin_yang
-    echo "removed .desktop files and config"
-fi
+echo "Removing config and .desktop file"
+rm -rf /home/$SUDO_USER/.local/share/applications/Yin-Yang.desktop
+rm -rf /home/$SUOD_USER/.config/yin_yang
 
-if [ "$1" != "root" ]
-  then
-    rm -rf /home/$1/.local/share/applications/Yin-Yang.desktop
-    rm -rf /home/$1/.config/yin_yang
-fi
+echo "Removing program and terminal execution"
+rm -rf /opt/yin-yang /usr/bin/yin-yang
 
-rm -rf /opt/yin-yang /usr/bin/yin-yang/ ~/.config/yin-yang/ /usr/bin/yin-yang
+echo "Removing manifest"
+rm -f /usr/lib/mozilla/native-messaging-hosts/yin_yang.json
 
 echo Yin-Yang uninstalled succesfully
 echo have a nice day ...
