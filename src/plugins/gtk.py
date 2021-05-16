aa1a20c397c837a3b0eb9106932959c4f763880b
l0drex <l0drex@pm.me>
2021-05-16 19:46:04 +0200

    Squash gtk and gtkkde into one plugin
    Using the new desktop dependent plugin as parent class

--- src/plugins/gtk.py
+++ src/plugins/gtk.py
@@ -1,17 +1,51 @@
 import subprocess
-from src import config
+from configparser import ConfigParser
+from pathlib import Path
+
+from src.plugins._plugin import PluginDesktopDependent, Plugin
 
 
-def switch_to_light():
-    gtk_theme = config.get("gtkLightTheme")
-    # gtk_theme = "Default"
-    # uses a kde api to switch to a light theme
-    subprocess.run(["gsettings", "set", "org.gnome.desktop.interface", "gtk-theme", gtk_theme]) # Applications theme
-    #subprocess.run(["gsettings", "set", "org.gnome.shell.extensions.user-theme", "name", '"{}"'.format(gtk_theme)]) # Shell theme
+class Gtk(PluginDesktopDependent):
+    name = 'GTK'
+
+    def __init__(self, desktop: str):
+        super().__init__(desktop)
+        if desktop == 'kde':
+            self.strategy_instance = Kde()
+        else:
+            self.strategy_instance = Gnome()
+
+    @property
+    def available(self) -> bool:
+        return self.strategy is not None
+
+    @property
+    def strategy(self):
+        return self.strategy_instance
 
 
-def switch_to_dark():
-    gtk_theme = config.get("gtkDarkTheme")
-    # uses a kde api to switch to a dark theme
-    subprocess.run(["gsettings", "set", "org.gnome.desktop.interface", "gtk-theme", gtk_theme]) # Applications theme
-    #subprocess.run(["gsettings", "set", "org.gnome.shell.extensions.user-theme", "name", '"{}"'.format(gtk_theme)]) # Shell theme
+class Gnome(Plugin):
+    # TODO set default theme names
+    theme_dark = ''
+    theme_bright = ''
+
+    def set_theme(self, theme: str):
+        # noinspection SpellCheckingInspection
+        subprocess.run(
+            ["gsettings", "set", "org.gnome.desktop.interface", "gtk-theme", theme])  # Applications theme
+
+
+class Kde(Plugin):
+    # Breeze theme uses qt color scheme
+    theme_bright = 'Breeze'
+    theme_dark = 'Breeze'
+
+    def set_theme(self, theme: str):
+        config = ConfigParser()
+        config_file = str(Path.home()) + "/.config/gtk-3.0/settings.ini"
+        config.read(config_file)
+
+        config['Settings']['gtk-theme-name'] = theme
+
+        with open(config_file, "w") as file:
+            config.write(file)
