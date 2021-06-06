251b969714ae33f19ad2cad7b0f89a73a3881916
l0drex <l0drex@pm.me>
2021-06-06 23:01:01 +0200

    Add test for setting an empty theme

--- tests/test_plugins.py
+++ tests/test_plugins.py
@@ -17,6 +17,21 @@ class PluginsTest(unittest.TestCase):
                     self.assertIsInstance(pl.available_themes, dict,
                                           'Available themes always should be a dict.')
 
+    def test_set_empty_theme(self):
+        for pl in plugins:
+            with self.subTest(plugin=pl):
+                try:
+                    pl.set_theme('')
+                except ValueError as e:
+                    self.assertEqual('Theme \"\" is invalid', str(e),
+                                     'set_theme() should throw an exception if the theme is empty')
+                    return
+
+                self.assertTrue(False,
+                                'set_theme() should throw an exception if the theme is empty!')
+                # try to return to previous theme
+                pl.set_theme(config.get(pl.name + config.get('theme').title() + 'Theme'))
+
     # NOTE if you want to test that your theme changes, set this value to true
     @unittest.skipUnless(False, 'test_theme_changes is disabled')
     def test_set_theme_works(self):
