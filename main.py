41b5b80b78f5bcf2ccc97ece8cf6bf42094383d5
l0drex <l0drex@pm.me>
2021-08-06 14:28:11 +0200

    Add a rotating file handler

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sys
 import logging
 from argparse import ArgumentParser
+from logging.handlers import TimedRotatingFileHandler, RotatingFileHandler
 from pathlib import Path
 
 from PyQt5 import QtWidgets
@@ -74,7 +75,7 @@ def main():
 
 
 if __name__ == "__main__":
-    # __debug__ is true when main.py is run without the -O argument.
+    # __debug__ is true when you run main.py without the -O argument (python main.py)
     if __debug__:
         # noinspection SpellCheckingInspection
         logging.basicConfig(
@@ -82,11 +83,18 @@ if __name__ == "__main__":
             format='%(asctime)s %(levelname)s - %(name)s: %(message)s'
         )
     else:
-        # logger to see what happens when application is running in background
+        # if you run it with "python -O main.py" instead, debug is false
+
+        # let the default logger print to the console
         # noinspection SpellCheckingInspection
         logging.basicConfig(
-            filename=str(Path.home()) + '/.local/share/yin_yang.log',
             level=logging.WARNING,
             format='%(asctime)s %(levelname)s - %(name)s: %(message)s'
         )
+        # and add a handler that limits the size to 1 GB
+        file_handler = RotatingFileHandler(
+            str(Path.home()) + '/.local/share/yin_yang.log',
+            maxBytes=10**9, backupCount=1
+        )
+        logging.root.addHandler(file_handler)
     main()
