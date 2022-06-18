f2e20c599eed78c785955a2c4d25e705f842e74f
DragonBillow <42114817+cathaysia@users.noreply.github.com>
2022-06-18 21:54:23 +0800

    feat: show all themes

--- src/plugins/kvantum.py
+++ src/plugins/kvantum.py
@@ -1,9 +1,19 @@
 from ._plugin import PluginCommandline, get_stuff_in_dir
 from pathlib import Path
+import os
 
 class Kvantum(PluginCommandline):
     def __init__(self):
         super().__init__(["kvantummanager", "--set", '%t'])
+
+    @classmethod
+    def get_kvantum_theme_from_dir(cls, dir):
+        result = set()
+        for _, _, filenames in os.walk(dir):
+            for filename in filenames:
+                if filename.endswith('.kvconfig'):
+                    result.add(filename[:-9])
+        return list(result)
 
     @property
     def available_themes(self) -> dict:
@@ -12,11 +22,8 @@ class Kvantum(PluginCommandline):
 
         paths = ['/usr/share/Kvantum', str(Path.home()) + '/.config/Kvantum']
         themes = list()
-        # At present, it seems that the function of finding themes is based
-        # on dirs, but .kvconfig. So some theme will no be recognized. This
-        # may be fixed next time
         for path in paths:
-            themes = themes + get_stuff_in_dir(path, type='dir')
+            themes = themes + self.get_kvantum_theme_from_dir(path)
         themes_dict: dict = {}
         assert len(themes) > 0, 'No themes were found'
 
