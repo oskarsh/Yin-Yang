7b0ef526479fb42196892ca0d7ada31917918ae2
l0drex <l0drex@pm.me>
2022-09-28 14:05:09 +0200

    Fix build_ui.sh and update associated files

--- resources/translations/yin_yang.de_DE.ts
+++ resources/translations/yin_yang.de_DE.ts
@@ -4,32 +4,32 @@
 <context>
     <name>MainWindow</name>
     <message>
-        <location filename="../../src/ui/config_window.py" line="35"/>
+        <location filename="../../src/ui/main_window_connector.py" line="70"/>
         <source>You are using version {}</source>
         <translation>Sie verwenden Version {}</translation>
     </message>
     <message>
-        <location filename="../../src/ui/config_window.py" line="128"/>
+        <location filename="../../src/ui/main_window_connector.py" line="171"/>
         <source>Dark mode will be active between {} and {}.</source>
         <translation>Dunkler Modus wird zwischen {} und {} aktiv sein.</translation>
     </message>
     <message>
-        <location filename="../../src/ui/config_window.py" line="216"/>
+        <location filename="../../src/ui/main_window_connector.py" line="235"/>
         <source>Open light wallpaper</source>
         <translation>Öffne helles Hintergrundbild</translation>
     </message>
     <message>
-        <location filename="../../src/ui/config_window.py" line="217"/>
+        <location filename="../../src/ui/main_window_connector.py" line="236"/>
         <source>Open dark wallpaper</source>
         <translation>Öffne dunkles Hintergrundbild</translation>
     </message>
     <message>
-        <location filename="../../src/ui/config_window.py" line="248"/>
+        <location filename="../../src/ui/main_window_connector.py" line="268"/>
         <source>The settings have been modified. Do you want to save them?</source>
         <translation>Die Einstellungen wurden geändert. Möchten Sie sie speichern?</translation>
     </message>
     <message>
-        <location filename="../../src/ui/config_window.py" line="248"/>
+        <location filename="../../src/ui/main_window_connector.py" line="268"/>
         <source>Unsaved changes</source>
         <translation>Ungespeicherte Änderungen</translation>
     </message>
