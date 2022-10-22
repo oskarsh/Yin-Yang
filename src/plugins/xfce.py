3e35c09f9330237c643670321182a601175f2bfb
l0drex <l0drex@pm.me>
2022-10-22 12:27:24 +0200

    Remove xfce since it is now a system plugin

--- src/plugins/xfce.py
+++
@@ -1,6 +0,0 @@
-from plugins._plugin import PluginCommandline
-
-
-class Xfce(PluginCommandline):
-    def __init__(self):
-        super(Xfce, self).__init__(['xfconf-query', '-c', 'xsettings', '-p', '/Net/ThemeName', '-s', '{theme}'])
