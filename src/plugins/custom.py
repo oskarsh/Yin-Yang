3fb991613f1f214883366a71e1df20c6e41ee93a
l0drex <l0drex@pm.me>
2022-10-22 13:19:08 +0200

    Add support for arguments

--- src/plugins/custom.py
+++ src/plugins/custom.py
@@ -1,3 +1,5 @@
+import re
+
 from PySide6.QtWidgets import QLineEdit
 
 from src.plugins._plugin import PluginCommandline
@@ -5,7 +7,13 @@ from src.plugins._plugin import PluginCommandline
 
 class Custom(PluginCommandline):
     def __init__(self):
-        super().__init__(['{theme}'])
+        super().__init__([])
+
+    def insert_theme(self, theme: str) -> list:
+        # splits at every non-escaped space
+        # src: https://stackoverflow.com/questions/18092354/python-split-string-without-splitting-escaped-character
+        command = re.split(r'(?<!\\) ', theme)
+        return command
 
     @property
     def available(self) -> bool:
