2f2e6006f2d31ae28b43408d1ca24841af15e79d
l0drex <l0drex@pm.me>
2023-03-07 23:35:05 +0100

    Improve label of automatic location update checkbox
    Fix #159

--- resources/translations/yin_yang.de_DE.ts
+++ resources/translations/yin_yang.de_DE.ts
@@ -4,32 +4,32 @@
 <context>
     <name>MainWindow</name>
     <message>
-        <location filename="../../src/ui/main_window_connector.py" line="70"/>
+        <location filename="../../src/ui/main_window_connector.py" line="71"/>
         <source>You are using version {}</source>
         <translation>Sie verwenden Version {}</translation>
     </message>
     <message>
-        <location filename="../../src/ui/main_window_connector.py" line="171"/>
+        <location filename="../../src/ui/main_window_connector.py" line="172"/>
         <source>Dark mode will be active between {} and {}.</source>
         <translation>Dunkler Modus wird zwischen {} und {} aktiv sein.</translation>
     </message>
     <message>
-        <location filename="../../src/ui/main_window_connector.py" line="235"/>
+        <location filename="../../src/ui/main_window_connector.py" line="237"/>
         <source>Open light wallpaper</source>
         <translation>Öffne helles Hintergrundbild</translation>
     </message>
     <message>
-        <location filename="../../src/ui/main_window_connector.py" line="236"/>
+        <location filename="../../src/ui/main_window_connector.py" line="238"/>
         <source>Open dark wallpaper</source>
         <translation>Öffne dunkles Hintergrundbild</translation>
     </message>
     <message>
-        <location filename="../../src/ui/main_window_connector.py" line="268"/>
+        <location filename="../../src/ui/main_window_connector.py" line="282"/>
         <source>The settings have been modified. Do you want to save them?</source>
         <translation>Die Einstellungen wurden geändert. Möchten Sie sie speichern?</translation>
     </message>
     <message>
-        <location filename="../../src/ui/main_window_connector.py" line="268"/>
+        <location filename="../../src/ui/main_window_connector.py" line="282"/>
         <source>Unsaved changes</source>
         <translation>Ungespeicherte Änderungen</translation>
     </message>
@@ -37,62 +37,77 @@
 <context>
     <name>main_window</name>
     <message>
-        <location filename="../../designer/main_window.ui" line="36"/>
+        <location filename="../../designer/main_window.ui" line="50"/>
         <source>Settings</source>
         <translation>Einstellungen</translation>
     </message>
     <message>
-        <location filename="../../designer/main_window.ui" line="42"/>
+        <location filename="../../designer/main_window.ui" line="56"/>
         <source>Automatic theme switching</source>
         <translation>Automatischer Themenwechsel</translation>
     </message>
     <message>
-        <location filename="../../designer/main_window.ui" line="62"/>
+        <location filename="../../designer/main_window.ui" line="76"/>
         <source>Custom Schedule</source>
         <translation>Benutzerdefinierter Zeitraum</translation>
     </message>
     <message>
-        <location filename="../../designer/main_window.ui" line="162"/>
+        <location filename="../../designer/main_window.ui" line="176"/>
         <source>Sunset to Sunrise</source>
         <translation>Sonnenaufgang bis Sonnenuntergang</translation>
     </message>
     <message>
-        <location filename="../../designer/main_window.ui" line="87"/>
+        <location filename="../../designer/main_window.ui" line="262"/>
+        <source>update location automatically</source>
+        <translation>Standort automatisch aktualisieren</translation>
+    </message>
+    <message>
+        <location filename="../../designer/main_window.ui" line="343"/>
+        <source>Sample Plugin</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../designer/main_window.ui" line="352"/>
+        <source>firefox-compact-light@mozilla.org</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../designer/main_window.ui" line="365"/>
+        <source>firefox-compact-dark@mozilla.org</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../designer/main_window.ui" line="101"/>
         <source>Light:</source>
         <translation>Hell:</translation>
     </message>
     <message>
-        <location filename="../../designer/main_window.ui" line="123"/>
+        <location filename="../../designer/main_window.ui" line="137"/>
         <source>Dark:</source>
         <translation>Dunkel:</translation>
     </message>
     <message>
-        <location filename="../../designer/main_window.ui" line="175"/>
+        <location filename="../../designer/main_window.ui" line="189"/>
         <source>Longitude:</source>
         <translation>Längengrad:</translation>
     </message>
     <message>
-        <location filename="../../designer/main_window.ui" line="210"/>
+        <location filename="../../designer/main_window.ui" line="224"/>
         <source>Latitude:</source>
         <translation>Breitengrad:</translation>
     </message>
     <message>
-        <location filename="../../designer/main_window.ui" line="248"/>
-        <source>update automatically</source>
-        <translation>Automatisch aktualisieren</translation>
-    </message>
-    <message>
-        <location filename="../../designer/main_window.ui" line="268"/>
+        <location filename="../../designer/main_window.ui" line="282"/>
         <source>Make a sound when switching the theme</source>
         <translation>Mache ein Geräusch, wenn das Thema geändert wird</translation>
     </message>
     <message>
-        <location filename="../../designer/main_window.ui" line="275"/>
+        <location filename="../../designer/main_window.ui" line="289"/>
         <source>Send a notification</source>
         <translation>Sende eine Benachrichtigung</translation>
     </message>
     <message>
-        <location filename="../../designer/main_window.ui" line="303"/>
+        <location filename="../../designer/main_window.ui" line="317"/>
         <source>Plugins</source>
         <translation></translation>
     </message>
