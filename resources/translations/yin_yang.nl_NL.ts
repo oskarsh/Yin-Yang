adba807c8d687ae3bac8631a5cbed789a5d48ef1
Heimen Stoffels <vistausss@fastmail.com>
2023-04-03 23:35:34 +0200

    Added Dutch translation

---
+++ resources/translations/yin_yang.nl_NL.ts
@@ -0,0 +1,115 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="nl" sourcelanguage="en_GB">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../../src/ui/main_window_connector.py" line="71"/>
+        <source>You are using version {}</source>
+        <translation>U maakt gebruik van versie {}</translation>
+    </message>
+    <message>
+        <location filename="../../src/ui/main_window_connector.py" line="172"/>
+        <source>Dark mode will be active between {} and {}.</source>
+        <translation>Het donkere thema wordt ingeschakeld van {} tot {}.</translation>
+    </message>
+    <message>
+        <location filename="../../src/ui/main_window_connector.py" line="236"/>
+        <source>Open light wallpaper</source>
+        <translation>Lichte achtergrond kiezen</translation>
+    </message>
+    <message>
+        <location filename="../../src/ui/main_window_connector.py" line="237"/>
+        <source>Open dark wallpaper</source>
+        <translation>Donkere achtergrond kiezen</translation>
+    </message>
+    <message>
+        <location filename="../../src/ui/main_window_connector.py" line="281"/>
+        <source>The settings have been modified. Do you want to save them?</source>
+        <translation>De instellingen zijn gewijzigd. Wilt u ze opslaan?</translation>
+    </message>
+    <message>
+        <location filename="../../src/ui/main_window_connector.py" line="281"/>
+        <source>Unsaved changes</source>
+        <translation>Niet-opgeslagen wijzigingen</translation>
+    </message>
+</context>
+<context>
+    <name>main_window</name>
+    <message>
+        <location filename="../../designer/main_window.ui" line="50"/>
+        <source>Settings</source>
+        <translation>Instellingen</translation>
+    </message>
+    <message>
+        <location filename="../../designer/main_window.ui" line="56"/>
+        <source>Automatic theme switching</source>
+        <translation>Licht/Donker thema automatisch instellen</translation>
+    </message>
+    <message>
+        <location filename="../../designer/main_window.ui" line="76"/>
+        <source>Custom Schedule</source>
+        <translation>Eigen tijdschema</translation>
+    </message>
+    <message>
+        <location filename="../../designer/main_window.ui" line="176"/>
+        <source>Sunset to Sunrise</source>
+        <translation>Van zonsopkomst tot zonsondergang</translation>
+    </message>
+    <message>
+        <location filename="../../designer/main_window.ui" line="101"/>
+        <source>Light:</source>
+        <translation>Licht:</translation>
+    </message>
+    <message>
+        <location filename="../../designer/main_window.ui" line="137"/>
+        <source>Dark:</source>
+        <translation>Donker:</translation>
+    </message>
+    <message>
+        <location filename="../../designer/main_window.ui" line="189"/>
+        <source>Longitude:</source>
+        <translation>Lengtegraad:</translation>
+    </message>
+    <message>
+        <location filename="../../designer/main_window.ui" line="224"/>
+        <source>Latitude:</source>
+        <translation>Breedtegraad:</translation>
+    </message>
+    <message>
+        <location filename="../../designer/main_window.ui" line="262"/>
+        <source>update automatically</source>
+        <translation>Automatisch bijwerken</translation>
+    </message>
+    <message>
+        <location filename="../../designer/main_window.ui" line="282"/>
+        <source>Make a sound when switching the theme</source>
+        <translation>Geluid afspelen na instellen van ander thema</translation>
+    </message>
+    <message>
+        <location filename="../../designer/main_window.ui" line="289"/>
+        <source>Send a notification</source>
+        <translation>Melding tonen</translation>
+    </message>
+    <message>
+        <location filename="../../designer/main_window.ui" line="317"/>
+        <source>Plugins</source>
+        <translation>Plug-ins</translation>
+    </message>
+    <message>
+        <location filename="../../designer/main_window.ui" line="343"/>
+        <source>Sample Plugin</source>
+        <translation>Voorbeeldplug-in</translation>
+    </message>
+    <message>
+        <location filename="../../designer/main_window.ui" line="352"/>
+        <source>firefox-compact-light@mozilla.org</source>
+        <translation>firefox-compact-licht@mozilla.org</translation>
+    </message>
+    <message>
+        <location filename="../../designer/main_window.ui" line="365"/>
+        <source>firefox-compact-dark@mozilla.org</source>
+        <translation>firefox-compact-donker@mozilla.org</translation>
+    </message>
+</context>
+</TS>
