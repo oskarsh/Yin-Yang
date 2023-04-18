a17855e01430219fd1bd0bc1a3fe0d55eea68277
l0drex <l0drex@pm.me>
2023-04-18 15:29:11 +0200

    Return to 3.2 version

--- src/config.py
+++ src/config.py
@@ -337,7 +337,7 @@ class ConfigManager(dict):
 
         # NOTE: if you change or add new values here, make sure to update the version number and update_config() method
         conf_default = {
-            'version': 3.3,
+            'version': 3.2,
             'running': False,
             'dark_mode': False,
             'mode': Modes.MANUAL.value,
