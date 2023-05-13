a15f64bea4d20518355b057d99d892a1d4d71b10
l0drex <l0drex@pm.me>
2023-05-13 15:56:56 +0200

    Fix old path in build_ui

--- scripts/build_ui.sh
+++ scripts/build_ui.sh
@@ -3,9 +3,9 @@
 # resource file
 pyside6-rcc ./resources/resources.qrc -o ./resources_rc.py
 # ui file from qt designer
-pyside6-uic ./designer/main_window.ui > ./src/ui/main_window.py
+pyside6-uic ./designer/main_window.ui > ./yin_yang/ui/main_window.py
 # extract strings to translate (doesn't work with .pro file unfortunately)
-pyside6-lupdate ./designer/main_window.ui ./src/ui/main_window_connector.py \
+pyside6-lupdate ./designer/main_window.ui ./yin_yang/ui/main_window_connector.py \
   -ts resources/translations/yin_yang.*.ts -no-obsolete
 # generate binary translation files
 pyside6-lrelease ./resources/translations/yin_yang.*.ts
