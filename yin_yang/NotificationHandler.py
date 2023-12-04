2e6ed1defa4e5741930d42d8e88b6840351510f5
Chase Lau <chase9@mac.com>
2023-12-04 14:32:29 -0600

    Fix notifications crashing application
    When notify-send is not available, the application would crash

--- yin_yang/NotificationHandler.py
+++ yin_yang/NotificationHandler.py
@@ -1,9 +1,14 @@
+import logging
 import subprocess
 from logging import Handler
 
+logger = logging.getLogger()
 
 class NotificationHandler(Handler):
     """Shows logs as notifications"""
     def emit(self, record):
-        subprocess.call(['notify-send', record.levelname, str(record.msg),
-                         '-a', 'Yin & Yang', '-u', 'low', '--icon', 'yin_yang'])
+        try:
+            subprocess.call(['notify-send', record.levelname, str(record.msg),
+                            '-a', 'Yin & Yang', '-u', 'low', '--icon', 'yin_yang'])
+        except FileNotFoundError:
+            logger.warn('notify-send not found. Notifications will not work!')
