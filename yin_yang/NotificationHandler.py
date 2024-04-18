cf2a4d3cf350913cfb2e93b7d78bc52d63ae0499
l0drex <l0drex@pm.me>
2024-04-18 21:56:42 +0200

    Use dbus for notifications

--- yin_yang/NotificationHandler.py
+++ yin_yang/NotificationHandler.py
@@ -1,9 +1,34 @@
-import subprocess
 from logging import Handler
+
+from PySide6.QtDBus import QDBusMessage, QDBusConnection
+
+
+def create_dbus_message(title: str, body: str):
+    message = QDBusMessage.createMethodCall(
+        'org.freedesktop.portal.Desktop',
+        '/org/freedesktop/portal/desktop',
+        'org.freedesktop.portal.Notification',
+        'AddNotification'
+    )
+
+    notification = {
+        'title': title,
+        'body': body,
+        'icon': 'yin_yang',
+        'priority': 'low',
+    }
+
+    message.setArguments([
+        'YingYang.ThemeChanged',
+        notification
+    ])
+
+    return message
 
 
 class NotificationHandler(Handler):
     """Shows logs as notifications"""
     def emit(self, record):
-        subprocess.call(['notify-send', record.levelname, str(record.msg),
-                         '-a', 'Yin & Yang', '-u', 'low', '--icon', 'yin_yang'])
+        connection = QDBusConnection.sessionBus()
+        message = create_dbus_message(record.levelname, str(record.msg))
+        connection.call(message)
